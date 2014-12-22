0d38e57fab95d262fa61e9b6a5df9b8799a1fc2f
rabbitfighter <rabbitfighter81@gmail.com>
2014-12-21 17:16:33 -0700

    Cube added

--- src/main/java/net/redrabbit/tesseract/Tesseract.java
+++ src/main/java/net/redrabbit/tesseract/Tesseract.java
@@ -12,19 +12,33 @@ import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
+/*
+* Projects a 4D tesseract onto a 2D screen. 
+* @author redragonx && rabbitfighter
+* Some sample code modified from lwjgl.org's example for a small lwjgl3 app.
+* All openGl code is original.
+*/
 public class Tesseract {
+
+    // Window & Animation stuff
+    public static final int window_width = 700;
+    public static final int window_height = 500;
+    public static final int fps = 30;
+    public int angle = 45;
 
     // We need to strongly reference callback instances.
     private GLFWErrorCallback errorCallback;
-    private GLFWKeyCallback   keyCallback;
+    private GLFWKeyCallback keyCallback;
 
     // The window handle
     private long window;
 
     public void run() {
-        System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
+
+        System.out.println("Hello Tesseract using LWJGL vers: " + Sys.getVersion() + "!");
 
         try {
+
             init();
             loop();
 
@@ -44,39 +58,42 @@ public class Tesseract {
         glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
 
         // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if ( glfwInit() != GL11.GL_TRUE )
+        if (glfwInit() != GL11.GL_TRUE)
             throw new IllegalStateException("Unable to initialize GLFW");
 
         // Configure our window
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden after creation
+        glfwDefaultWindowHints(); // optional, the current window hints are
+                                    // already the default
+        glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden
+                                                // after creation
         glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); // the window will be resizable
 
-        int WIDTH = 300;
-        int HEIGHT = 300;
-
         // Create the window
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
-        if ( window == NULL )
+        window = glfwCreateWindow(window_width, window_height, "Tesseract",
+                NULL, NULL);
+        if (window == NULL)
             throw new RuntimeException("Failed to create the GLFW window");
 
-        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        // Setup a key callback. It will be called every time a key is pressed,
+        // repeated or released.
         glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
             @Override
-            public void invoke(long window, int key, int scancode, int action, int mods) {
-                if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-                    glfwSetWindowShouldClose(window, GL_TRUE); // We will detect this in our rendering loop
+            public void invoke(long window, int key, int scancode, int action,
+                    int mods) {
+                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                    glfwSetWindowShouldClose(window, GL_TRUE); // We will detect
+                                                                // this in our
+                                                                // rendering
+                                                                // loop
             }
         });
 
         // Get the resolution of the primary monitor
         ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
         // Center our window
-        glfwSetWindowPos(
-            window,
-            (GLFWvidmode.width(vidmode) - WIDTH) / 2,
-            (GLFWvidmode.height(vidmode) - HEIGHT) / 2
-        );
+        glfwSetWindowPos(window,
+                (GLFWvidmode.width(vidmode) - window_width) / 2,
+                (GLFWvidmode.height(vidmode) - window_height) / 2);
 
         // Make the OpenGL context current
         glfwMakeContextCurrent(window);
@@ -85,6 +102,7 @@ public class Tesseract {
 
         // Make the window visible
         glfwShowWindow(window);
+
     }
 
     private void loop() {
@@ -95,25 +113,137 @@ public class Tesseract {
         // bindings available for use.
         GLContext.createFromCurrent();
 
-        // Set the clear color
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        // Set the clear color to soft black
+        glClearColor(0.04f, 0.04f, 0.04f, 0.04f);
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
-        while ( glfwWindowShouldClose(window) == GL_FALSE ) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+        while (glfwWindowShouldClose(window) == GL_FALSE) {
 
-            glfwSwapBuffers(window); // swap the color buffers
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the       
 
+            // ********************* Open GL Stuff *************************
+
+            float ratio;
+            ratio = window_width / (float) window_height;
+            glViewport(0, 0, window_width, window_height);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+            // Matrix block
+            glPushMatrix();
+            {
+                glRotatef((float) glfwGetTime() * 50.f, 25.f, 25.f, 1.f);
+                glScaled(.5, .5, .5);
+                standardCube();
+            }
+            glPopMatrix();
+            //End matrix block
+            glfwSwapBuffers(window);
             // Poll for window events. The key callback above will only be
             // invoked during this call.
             glfwPollEvents();
+
+            // ******************** End Open GL Stuff *********************
+
         }
     }
 
     public static void main(String[] args) {
-        System.setProperty("org.lwjgl.librarypath", new File("libs/natives").getAbsolutePath());
+        System.setProperty("org.lwjgl.librarypath",
+                new File("libs/natives").getAbsolutePath());
         new Tesseract().run();
     }
 
-}
+    // ************************** Cube ************************************
+
+    /**
+     * Draws a standard cube
+     */
+    public void standardCube() {
+
+        // front face
+        glColor3f(1.0f, 1.0f, 0.0f);
+        glBegin(GL_LINE_LOOP);
+        glTexCoord2f(0, 0);
+        glVertex3d(-1, -1, -1);
+        glTexCoord2f(.5f, 0);
+        glVertex3d(1, -1, -1);
+        glTexCoord2f(.5f, .5f);
+        glVertex3d(1, -1, 1);
+        glTexCoord2f(0, .5f);
+        glVertex3d(-1, -1, 1);
+        glEnd();
+
+        // rear face
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glBegin(GL_LINE_LOOP);
+        glTexCoord2f(0, 0);
+        glVertex3d(1, 1, -1);
+        glTexCoord2f(.5f, 0);
+        glVertex3d(-1, 1, -1);
+        glTexCoord2f(.5f, .5f);
+        glVertex3d(-1, 1, 1);
+        glTexCoord2f(0, .5f);
+        glVertex3d(1, 1, 1);
+        glEnd();
+
+        // right face
+        glColor3f(0.5f, 1.0f, 0.5f);
+        glBegin(GL_LINE_LOOP);
+        glTexCoord2f(0, 0);
+        glVertex3d(1, -1, -1);
+        glTexCoord2f(.5f, 0);
+        glVertex3d(1, 1, -1);
+        glTexCoord2f(.5f, .5f);
+        glVertex3d(1, 1, 1);
+        glTexCoord2f(0, .5f);
+        glVertex3d(1, -1, 1);
+        glEnd();
+
+        // left face
+        glColor3f(1.0f, 0.0f, 1.0f);
+        glBegin(GL_LINE_LOOP);
+        glTexCoord2f(0, 0);
+        glVertex3d(-1, 1, -1);
+        glTexCoord2f(.5f, 0);
+        glVertex3d(-1, -1, -1);
+        glTexCoord2f(.5f, .5f);
+        glVertex3d(-1, -1, 1);
+        glTexCoord2f(0, .5f);
+        glVertex3d(-1, 1, 1);
+        glEnd();
+
+        // top face
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glBegin(GL_LINE_LOOP);
+        glTexCoord2f(0, 0);
+        glVertex3d(-1, -1, 1);
+        glTexCoord2f(.75f, 0);
+        glVertex3d(1, -1, 1);
+        glTexCoord2f(.75f, .5f);
+        glVertex3d(1, 1, 1);
+        glTexCoord2f(0, .5f);
+        glVertex3d(-1, 1, 1);
+        glEnd();
+
+        // bottom face
+        glColor3f(0.0f, 1.0f, 1.0f);
+        glBegin(GL_LINE_LOOP);
+        glTexCoord2f(.75f, 0);
+        glVertex3d(-1, -1, -1);
+        glTexCoord2f(.75f, .5f);
+        glVertex3d(-1, 1, -1);
+        glTexCoord2f(0, .5f);
+        glVertex3d(1, 1, -1);
+        glTexCoord2f(0, 0);
+        glVertex3d(1, -1, -1);
+        glEnd();
+
+    }// End standardCube
+
+}// EOF
+
