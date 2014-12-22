3f30d0b646ea4b3ae4841b96e3940273102d2b7c
rabbitfighter <rabbitfighter81@gmail.com>
2014-12-21 21:01:54 -0700

    Connected vertices

--- src/main/java/net/redrabbit/tesseract/Tesseract.java
+++ src/main/java/net/redrabbit/tesseract/Tesseract.java
@@ -141,6 +141,7 @@ public class Tesseract {
                 glScaled(.5, .5, .5);
                 standardCube();
                 insideCube();
+                connectVertices();
             }
             glPopMatrix();
             // End matrix block
@@ -160,6 +161,202 @@ public class Tesseract {
         new Tesseract().run();
     }
 
+    /*
+    * Connects the verices of the tesseract
+    */
+    public void connectVertices() {
+
+        //Bottom face
+
+        // 1) connect inner FrontLeft-Bottom to outer FrontLeft-Bottom
+        glLineWidth(1.0f);
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, -1, -1);
+            glVertex3d(-.5, -.5, -.5);
+        }
+        glEnd();
+        // 2) connect inner FrontRight-Botttom to outer FrontRight-Bottom
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, -1, -1);
+            glVertex3d(.5f, -.5f, -.5f);
+        }
+        glEnd();
+        // 3) connect inner BackRight-Bottom to outer BackRight-Bottom
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, -1, 1);
+            glVertex3d(.5f, -.5f, .5f);
+        }
+        glEnd();
+        // 4) connect inner BackL-Bottom to outer BackL-Bottom
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, -1, 1);
+            glVertex3d(-.5f, -.5f, .5f);
+        }
+        glEnd();
+
+
+        // Top face
+
+        // 1) connect inner FrontR-Top to outer FrontR-Top
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, 1, -1);
+            glVertex3d(.5f, .5f, -.5f);
+        }
+        glEnd();
+        // 2) connect inner FrontL-Top to outer FrontL-Top
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, 1, -1);
+            glVertex3d(-.5f, .5f, -.5f);
+        }
+        glEnd();
+        // 3) connect inner BackL-Top to outer BackL-Top
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, 1, 1);
+            glVertex3d(-.5f, .5f, .5f);
+        }
+        glEnd();
+        // 4) connect inner BackR-Top to outer BackR-Top
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, 1, 1);
+            glVertex3d(.5f, .5f, .5f);
+        }
+        glEnd();
+
+        // right face
+
+        // 1) connect inner LowerFront-Right tto outer LowerFront-Right
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, -1, -1);
+            glVertex3d(.5f, -.5f, -.5f);
+        }
+        glEnd();
+        // 2) connect inner UpperFront-Right to outer UpperFront-Right
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, 1, -1);
+            glVertex3d(.5f, .5f, -.5f);
+        }
+        glEnd();
+        // 3) connect inner UpperRear-Right to outer UpperRear-Right
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, 1, 1);
+            glVertex3d(.5f, .5f, .5f);
+        }
+        glEnd();
+        // 4) connect inner LowerRear-Right to outer LowerRear-Right
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, -1, 1);
+            glVertex3d(.5f, -.5f, .5f);
+        }
+        glEnd();
+
+        // left face
+
+        // 1) connect inner UpperFront-Left tto outter UpperFront-Left
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, 1, -1);
+            glVertex3d(-.5f, .5f, -.5f);
+        }
+        glEnd();
+        // 2) connect inner LowerFront-Left to outer LowerFront-Left
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, -1, -1);
+            glVertex3d(-.5f, -.5f, -.5f);
+        }
+        glEnd();
+        // 3) connect inner LowerRear-Left to outer LowerRear-Left
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, -1, 1);
+            glVertex3d(-.5f, -.5f, .5f);
+        }
+        glEnd();
+        // 4) connect inner UpperRear-Left to outer UpperRear-Left
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, 1, 1);
+            glVertex3d(-.5f, .5f, .5f);
+        }
+        glEnd();
+
+        // front face
+
+        // 1) connect inner LowerLeft-Front to outter LowerLeft-Front
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, -1, 1);
+            glVertex3d(-.5f, -.5f, .5f);
+        }
+        glEnd();
+        // 2) connect inner LowerRight-Front to outer LowerRight-Front
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, -1, 1);
+            glVertex3d(.5f, -.5f, .5f);
+        }
+        glEnd();
+        // 3) connect inner UpperRight-Front to outer UpperRight-Front
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, 1, 1);
+            glVertex3d(.5f, .5f, .5f);
+        }
+        glEnd();
+        // 4) connect inner UpperLeft-Front to outer UpperLeft-Front
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, 1, 1);
+            glVertex3d(-.5f, .5f, .5f);
+        }
+        glEnd();
+
+        // back face
+
+        // 1) connect inner LowerLeft-Back to outter LowerLeft-Back
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, -1, -1);
+            glVertex3d(-.5f, -.5f, -.5f);
+        }
+        glEnd();
+        // 2) connect inner UpperLeft-Back to outer UpperLeft-Back
+        glBegin(GL_LINES);
+        {
+            glVertex3d(-1, 1, -1);
+            glVertex3d(-.5f, .5f, -.5f);
+        }
+        glEnd();
+        // 3) connect inner UpperRight-Back ttto outer UpperRight-Back
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, 1, -1);
+            glVertex3d(.5f, .5f, -.5f);
+        }
+        glEnd();
+        // 4) finally, connect inner LowerRight-Back to outer LowerRight-Back
+        glBegin(GL_LINES);
+        {
+            glVertex3d(1, -1, -1);
+            glVertex3d(.5f, -.5f, -.5f);
+        }
+        glEnd();
+
+    }
+
     // ************************** Cube ************************************
 
     /**
@@ -167,104 +364,110 @@ public class Tesseract {
      */
     public void standardCube() {
 
-        // front face
-        glColor3f(1.0f, 1.0f, 0.0f);
-        glBegin(GL_LINE_LOOP);
-        glTexCoord2f(0, 0);
-        glVertex3d(-1, -1, -1);
-        glTexCoord2f(.5f, 0);
-        glVertex3d(1, -1, -1);
-        glTexCoord2f(.5f, .5f);
-        glVertex3d(1, -1, 1);
-        glTexCoord2f(0, .5f);
-        glVertex3d(-1, -1, 1);
-        glEnd();
+        // bottom face
 
-        // rear face
-        glColor3f(0.0f, 1.0f, 0.0f);
+        glColor3f(1.0f, 1.0f, 1.0f);
         glBegin(GL_LINE_LOOP);
         glTexCoord2f(0, 0);
-        glVertex3d(1, 1, -1);
+        glVertex3d(-1, -1, -1);//FrontL-Bottom
         glTexCoord2f(.5f, 0);
-        glVertex3d(-1, 1, -1);
+        glVertex3d(1, -1, -1);//FrontR-Bottom
         glTexCoord2f(.5f, .5f);
-        glVertex3d(-1, 1, 1);
+        glVertex3d(1, -1, 1);//BackR-Bottom
         glTexCoord2f(0, .5f);
-        glVertex3d(1, 1, 1);
-        glEnd();
-
-        // right face
-        glColor3f(0.5f, 1.0f, 0.5f);
-        glBegin(GL_LINE_LOOP);
-        glTexCoord2f(0, 0);
-        glVertex3d(1, -1, -1);
-        glTexCoord2f(.5f, 0);
-        glVertex3d(1, 1, -1);
-        glTexCoord2f(.5f, .5f);
-        glVertex3d(1, 1, 1);
-        glTexCoord2f(0, .5f);
-        glVertex3d(1, -1, 1);
-        glEnd();
-
-        // left face
-        glColor3f(1.0f, 0.0f, 1.0f);
-        glBegin(GL_LINE_LOOP);
-        glTexCoord2f(0, 0);
-        glVertex3d(-1, 1, -1);
-        glTexCoord2f(.5f, 0);
-        glVertex3d(-1, -1, -1);
-        glTexCoord2f(.5f, .5f);
-        glVertex3d(-1, -1, 1);
-        glTexCoord2f(0, .5f);
-        glVertex3d(-1, 1, 1);
+        glVertex3d(-1, -1, 1);//BackL-Bottom
         glEnd();
 
         // top face
-        glColor3f(0.0f, 0.0f, 1.0f);
+
+        //glColor3f(0.0f, 1.0f, 0.0f);
         glBegin(GL_LINE_LOOP);
         glTexCoord2f(0, 0);
-        glVertex3d(-1, -1, 1);
-        glTexCoord2f(.75f, 0);
-        glVertex3d(1, -1, 1);
-        glTexCoord2f(.75f, .5f);
-        glVertex3d(1, 1, 1);
+        glVertex3d(1, 1, -1);//FrontR-Top
+        glTexCoord2f(.5f, 0);
+        glVertex3d(-1, 1, -1);//FrontL-Top
+        glTexCoord2f(.5f, .5f);
+        glVertex3d(-1, 1, 1);//BackL-Top
         glTexCoord2f(0, .5f);
-        glVertex3d(-1, 1, 1);
+        glVertex3d(1, 1, 1);//BackR-Top
         glEnd();
 
-        // bottom face
-        glColor3f(0.0f, 1.0f, 1.0f);
+        // right face
+
+        //glColor3f(0.5f, 1.0f, 0.5f);
+        glBegin(GL_LINE_LOOP);
+        glTexCoord2f(0, 0);
+        glVertex3d(1, -1, -1);//LowerFront-Right
+        glTexCoord2f(.5f, 0);
+        glVertex3d(1, 1, -1);//UpperFront-Right
+        glTexCoord2f(.5f, .5f);
+        glVertex3d(1, 1, 1);//UpperRear-Right
+        glTexCoord2f(0, .5f);
+        glVertex3d(1, -1, 1);//LowerRear-Right
+        glEnd();
+
+        // left face
+
+        //glColor3f(1.0f, 0.0f, 1.0f);
+        glBegin(GL_LINE_LOOP);
+        glTexCoord2f(0, 0);
+        glVertex3d(-1, 1, -1);//UpperFront-Left
+        glTexCoord2f(.5f, 0);
+        glVertex3d(-1, -1, -1);//LowerFront-Left
+        glTexCoord2f(.5f, .5f);
+        glVertex3d(-1, -1, 1);//LowerRear-Left
+        glTexCoord2f(0, .5f);
+        glVertex3d(-1, 1, 1);//UpperRear-Left
+        glEnd();
+
+        // front face
+
+        //glColor3f(0.0f, 0.0f, 1.0f);
+        glBegin(GL_LINE_LOOP);
+        glTexCoord2f(0, 0);
+        glVertex3d(-1, -1, 1);//LowerLeft-Front
+        glTexCoord2f(.75f, 0);
+        glVertex3d(1, -1, 1);//LowerRight-Front
+        glTexCoord2f(.75f, .5f);
+        glVertex3d(1, 1, 1);//UpperRight-Front
+        glTexCoord2f(0, .5f);
+        glVertex3d(-1, 1, 1);//UpperLeft-Front
+        glEnd();
+
+        // back face
+        //glColor3f(0.0f, 1.0f, 1.0f);
         glBegin(GL_LINE_LOOP);
         glTexCoord2f(.75f, 0);
-        glVertex3d(-1, -1, -1);
+        glVertex3d(-1, -1, -1);//LowerLeft-Back
         glTexCoord2f(.75f, .5f);
-        glVertex3d(-1, 1, -1);
+        glVertex3d(-1, 1, -1);//UpperLeft-Back
         glTexCoord2f(0, .5f);
-        glVertex3d(1, 1, -1);
+        glVertex3d(1, 1, -1);//UpperRight-Back
         glTexCoord2f(0, 0);
-        glVertex3d(1, -1, -1);
+        glVertex3d(1, -1, -1);//LowerRight-Back
         glEnd();
+
 
     }// End standardCube
 
     public void insideCube() {
         // front face
-        glColor3f(1.0f, 1.0f, 0.0f);
+        //glColor3f(1.0f, 1.0f, 0.0f);
         glBegin(GL_LINE_LOOP);
         glTexCoord2f(0, 0);
-        glVertex3d(-.5f, -.5f, -.5f);
+        glVertex3d(-.5f, -.5f, -.5f);//FrontL-Bottom
         glTexCoord2f(.25f, 0);
-        glVertex3d(.5f, -.5f, -.5f);
+        glVertex3d(.5f, -.5f, -.5f);//FrontR-Bottom
         glTexCoord2f(.25f, .25f);
-        glVertex3d(.5f, -.5f, .5f);
+        glVertex3d(.5f, -.5f, .5f);//BackR-Bottom
         glTexCoord2f(0, .25f);
-        glVertex3d(-.5f, -.5f, .5f);
+        glVertex3d(-.5f, -.5f, .5f);//BackL-Bottom
         glEnd();
 
         //
 
         // rear face
-        glColor3f(0.0f, .5f, 0.0f);
+        //glColor3f(0.0f, .5f, 0.0f);
         glBegin(GL_LINE_LOOP);
         glTexCoord2f(0, 0);
         glVertex3d(.5f, .5f, -.5f);
@@ -279,7 +482,7 @@ public class Tesseract {
         //
 
         // right face
-        glColor3f(0.25f, 0.5f, 0.25f);
+        //glColor3f(0.25f, 0.5f, 0.25f);
         glBegin(GL_LINE_LOOP);
         glTexCoord2f(0, 0);
         glVertex3d(.5f, -.5f, -.5f);
@@ -294,7 +497,7 @@ public class Tesseract {
         //
 
         // left face
-        glColor3f(.5f, 0.0f, .5f);
+        //glColor3f(.5f, 0.0f, .5f);
         glBegin(GL_LINE_LOOP);
         glTexCoord2f(0, 0);
         glVertex3d(-.5f, .5f, -.5f);
@@ -309,7 +512,7 @@ public class Tesseract {
         //
 
         // top face
-        glColor3f(0.0f, 0.0f, .5f);
+        //glColor3f(0.0f, 0.0f, .5f);
         glBegin(GL_LINE_LOOP);
         glTexCoord2f(0, 0);
         glVertex3d(-.5f, -.5f, .5f);
@@ -324,7 +527,7 @@ public class Tesseract {
         //
 
         // bottom face
-        glColor3f(0.0f, .5f, .5f);
+        //glColor3f(0.0f, .5f, .5f);
         glBegin(GL_LINE_LOOP);
         glTexCoord2f(.375f, 0);
         glVertex3d(-.5f, -.5f, -.5f);
